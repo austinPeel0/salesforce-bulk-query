a21972de787ce748239d33a8f0d75aae7a6a31fd
Austin Peel <austin.peel@gsa.gov>
2019-07-17 10:33:53 -0400

    TST:test finished

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 
 
 credentials = {
-    "grant_type": "password",
-    "client_id": "", # Consumer Key
-    "client_secret": "", # Consumer Secret
-    "username": "", # The email you use to login
-    "password": "" # Concat your password and your security token
+    'test'
 }
   
-base_url = 'https://salesforce.com/services'
+
+base_url = 'testy'
